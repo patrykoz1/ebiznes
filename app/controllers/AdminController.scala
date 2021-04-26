0cb3e8e55252699834bb1210e1d667031c7434d8
patrykoz1 <patrykoz1@wp.pl>
2021-04-27 01:05:37 +0200

    first commit

---
+++ app/controllers/AdminController.scala
@@ -0,0 +1,23 @@
+package controllers
+
+import play.api.mvc.{AbstractController, ControllerComponents}
+
+import javax.inject.Inject
+
+class AdminController @Inject()(cc: ControllerComponents) extends AbstractController(cc) {
+
+
+  def index = Action {
+    Ok(views.html.index("GET."))
+  }
+  def delete = Action {
+    Ok(views.html.index("DELETE."))
+  }
+  def patch = Action {
+    Ok(views.html.index("PATCH."))
+  }
+  def add = Action {
+    Ok(views.html.index("POST"))
+  }
+
+}
---
+++ app/controllers/PurchaseController.scala
@@ -0,0 +1,22 @@
+package controllers
+
+import play.api.mvc.{AbstractController, ControllerComponents}
+
+import javax.inject.Inject
+
+class PurchaseController @Inject()(cc: ControllerComponents) extends AbstractController(cc) {
+
+
+  def index = Action {
+    Ok(views.html.index("GET."))
+  }
+  def delete = Action {
+    Ok(views.html.index("DELETE."))
+  }
+  def patch = Action {
+    Ok(views.html.index("PATCH."))
+  }
+  def add = Action {
+    Ok(views.html.index("POST"))
+  }
+}
