79cfd18b5457a2f3b6aa5e7bc27e2c07525bc489
patrykoz1 <49210844+patrykoz1@users.noreply.github.com>
2021-07-19 22:02:46 +0200

    Update SocialAuthController.scala

--- app/controllers/SocialAuthController.scala
+++ app/controllers/SocialAuthController.scala
@@ -1,28 +1,26 @@
 package controllers
 
+import akka.parboiled2.RuleTrace.Action
 import com.mohiva.play.silhouette.api.exceptions.ProviderException
 import com.mohiva.play.silhouette.impl.providers._
-import controllers.auth.DefaultSilhouetteControllerComponents
-import models.User
+import play.api.mvc.Results.Redirect
 
 import javax.inject.Inject
 import play.api.mvc.{Action, AnyContent, Cookie, Request}
 import play.filters.csrf.CSRF.Token
 import play.filters.csrf.{CSRF, CSRFAddToken}
-
 import scala.concurrent.{ExecutionContext, Future}
-
 class SocialAuthController @Inject()(scc: DefaultSilhouetteControllerComponents, addToken: CSRFAddToken)(implicit ex: ExecutionContext) extends SilhouetteController(scc) {
-
   def authenticate(provider: String): Action[AnyContent] = addToken(Action.async { implicit request: Request[AnyContent] =>
     (socialProviderRegistry.get[SocialProvider](provider) match {
       case Some(p: SocialProvider with CommonSocialProfileBuilder) =>
         p.authenticate().flatMap {
           case Left(result) => Future.successful(result)
           case Right(authInfo) => for {
+        
             profile <- p.retrieveProfile(authInfo)
-            res <- userRepository.getByEmail(profile.email.getOrElse(""))
-            user <- if (res.orNull == null) userRepository.create(profile.loginInfo.providerID, profile.loginInfo.providerKey, profile.email.getOrElse("")) else userRepository.getByEmail(profile.email.getOrElse(""))
+            _ <- userRepository.create(profile.loginInfo.providerID, profile.loginInfo.providerKey, profile.email.getOrElse(""))
+           
             _ <- authInfoRepository.save(profile.loginInfo, authInfo)
             authenticator <- authenticatorService.create(profile.loginInfo)
             value <- authenticatorService.init(authenticator)
